540194a9bfe637e9c3e8d23be66bd223c6b2b232
Niels Audoor <29575824+NielsAudoor@users.noreply.github.com>
2020-10-07 10:35:37 +0200

    Hide/View Password

--- script/app.js
+++ script/app.js
@@ -1,13 +1,21 @@
 function handleFloatingLabel() {
-   
+
 }
 
 function handlePasswordSwitcher() {
-    
+    var passwordToggle = document.getElementById('togglePassword')
+    const passwordOptions = ['password', 'text'];
+    var passwordInput = document.getElementById('password')
+
+    passwordToggle.addEventListener('change', function () {
+        passwordInput.type = passwordOptions[+this.checked];
+        console.log(this.checked)
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Script loaded!');
+
     handleFloatingLabel();
     handlePasswordSwitcher();
 });
